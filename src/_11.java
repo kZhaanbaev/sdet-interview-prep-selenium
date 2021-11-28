23cf6aa19e48db59bb97f178d651cb90a18ea8ae
Kuba Zhaanbaev <k.zhaanbaev4@gmail.com>
2021-11-28 00:06:52 -0500

    initial

---
+++ src/_11.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//How do I launch the browser using WebDriver?
+public class _11 {
+    public static void main(String[] args) {
+        /*
+        If there is no additional libraries like maven are used,
+        we need to connect selenium packages manually.
+        File -> Project Structure -> Modules -> + -> JARs or Directories
+         */
+
+        //setting up the driver to use chrome browser
+        System.setProperty("webdriver.chrome.driver", "_drivers/browser-drivers/windows/chromedriver.exe");
+
+        //creating an object of the chrome driver to launch a browser
+        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new FirefoxDriver();
+        driver.get("https://google.com");
+
+    }
+}
